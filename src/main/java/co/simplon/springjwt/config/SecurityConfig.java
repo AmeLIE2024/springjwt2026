142178b71196c8e77cd876e13ce65353a13742aa
CAILLET Amelie <185417630+AmeLIE2024@users.noreply.github.com>
2026-08-24 15:41:00 +0200

    add jwt

--- src/main/java/co/simplon/springjwt/config/SecurityConfig.java
+++ src/main/java/co/simplon/springjwt/config/SecurityConfig.java
@@ -1,5 +1,6 @@
 package co.simplon.springjwt.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -11,15 +12,26 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
+import org.springframework.security.oauth2.jwt.JwtEncoder;
+import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
+import org.springframework.security.oauth2.jwt.NimbusJwtEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
+import com.nimbusds.jose.jwk.source.ImmutableSecret;
+
 import co.simplon.springjwt.service.AuthService;
+
+import javax.crypto.spec.SecretKeySpec;
 
 @Configuration
 @EnableMethodSecurity
 public class SecurityConfig {
 
     private final UserDetailsService userDetailsService;
+
+    @Value("${jwt.secret}")
+    private String jwtSecret;
 
     public SecurityConfig(AuthService authServiceInjected) {
         this.userDetailsService = authServiceInjected;
@@ -47,7 +59,21 @@ public class SecurityConfig {
                         // Authentification requise pour tout le reste
                         .anyRequest().authenticated())
                 .sessionManagement((session) -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
-                .httpBasic(Customizer.withDefaults())
+                // Authentification par JWT
+                .oauth2ResourceServer(oauth2 -> oauth2.jwt(Customizer.withDefaults()))
                 .build();
+    }
+
+    @Bean
+    public JwtEncoder jwtEncoder() {
+        var key = new SecretKeySpec(jwtSecret.getBytes(), "HmacSHA256");
+        var immutableSecret = new ImmutableSecret<>(key);
+        return new NimbusJwtEncoder(immutableSecret);
+    }
+
+    @Bean
+    public JwtDecoder jwtDecoder() {
+        var originalKey = new SecretKeySpec(jwtSecret.getBytes(), "HmacSHA256");
+        return NimbusJwtDecoder.withSecretKey(originalKey).build();
     }
 }
