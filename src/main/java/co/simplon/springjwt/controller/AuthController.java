f610544f23549889a40f61fbb8ea5429235508bc
97692T <a.cailletrichard@gmail.com>
2026-08-25 09:41:52 +0200

    Gestion-des-CORS

--- src/main/java/co/simplon/springjwt/controller/AuthController.java
+++ src/main/java/co/simplon/springjwt/controller/AuthController.java
@@ -1,5 +1,6 @@
 package co.simplon.springjwt.controller;
 
+import co.simplon.springjwt.dto.LoginDto;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
@@ -37,13 +38,14 @@ public class AuthController {
     }
 
     @PostMapping("/login")
-    public String login(@RequestBody UserEntity user) {
+    public LoginDto login(@RequestBody UserEntity user) {
 
         Authentication auth = this.authManager.authenticate(new UsernamePasswordAuthenticationToken(
                 user.getUsername(), user.getPassword()));
         String token = tokenService.generateToken(auth);
 
-        return token;
+        UserEntity userConnected = (UserEntity) auth.getPrincipal();
+        return new LoginDto(token,userConnected.getUsername());
     }
 
     @PostMapping("/register")
---
+++ src/main/java/co/simplon/springjwt/dto/LoginDto.java
@@ -0,0 +1,4 @@
+package co.simplon.springjwt.dto;
+
+public record LoginDto(String token, String username) {
+}
